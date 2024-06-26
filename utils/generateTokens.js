2bd9b980f7f8538af98a700ab184a794153cd331
Prashant Maurya <mauryaprashant1510@gmail.com>
2024-06-26 20:09:58 +0530

    secure commented

--- utils/generateTokens.js
+++ utils/generateTokens.js
@@ -9,7 +9,7 @@ const generateTokenAndSetCookie = (userId, userType, res) => {
     maxAge: 15*24*60*60*1000,
     httpOnly:true,           // prevent cross-site scripting attacks
     // sameSite:"strict",
-    secure: process.env.NODE_ENV !== "development",
+    // secure: process.env.NODE_ENV !== "development",
   })
 };
 export default generateTokenAndSetCookie;

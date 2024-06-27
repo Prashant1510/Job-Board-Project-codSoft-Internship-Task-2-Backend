eb4a33607d20af9fa07a4803649c6c64e97068d5
Prashant Maurya <mauryaprashant1510@gmail.com>
2024-06-27 11:50:40 +0530

    removed origin

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: '*', credentials: true }));
+app.use(cors({ credentials: true }));
 
 app.use("/api/userauth", userAuthRoutes);
 app.use("/api/companyauth", companyAuthRoutes);
